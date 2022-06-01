11dcb267090778a935129bc725218ae9d5e47805
LucasMendes013 <104436533+LucasMendes013@users.noreply.github.com>
2022-06-01 11:13:42 -0300

    main.js

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play()
+function tocaSom(idElementoAudio){
+    
+    document.querySelector(idElementoAudio).play()
 }
 
-
-
 const  listaDeTeclas = document.querySelectorAll('.tecla')
+let contador = 0
+
+
+
+//enquanto
+while(contador < listaDeTeclas.length) {
+    listaDeTeclas[contador].onclick = function(){
+        tocaSom('#som_tecla_pom')
+    }
+    contador++
+
+    console.log(contador)
+}
