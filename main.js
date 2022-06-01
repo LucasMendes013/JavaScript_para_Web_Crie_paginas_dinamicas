ace00f5aa492bb0f21440140187d3386703faed1
LucasMendes013 <104436533+LucasMendes013@users.noreply.github.com>
2022-06-01 12:46:44 -0300

    commit

--- main.js
+++ main.js
@@ -19,6 +19,13 @@ for (let contador = 0; contador < listaDeTeclas.length;contador++ ) {
         tocaSom(idAudio)
     }
     
-
-    console.log(contador)
+    tecla.onkeydown = function(evento){
+        console.log(evento.code)
+        if(evento.code === 'Space'){
+        tecla.classList.add('ativa')
+        }
+    }
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa')
+    }
 }
